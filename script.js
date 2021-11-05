b4cf827186e1a5b6644094f8dd072578cc93e4be
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-11-05 20:03:40 -0300

    add functions

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function insert(key) {
 function clearScreen() {
   document.getElementById('result').innerHTML = ''
 }
+
+function back() {
+  let result = document.getElementById('result').innerHTML
+  document.getElementById('result').innerHTML = result.substring(
+    0,
+    result.length - 1
+  )
+}
+
+function calculate() {
+  let result = document.getElementById('result').innerHTML
+  if (result) {
+    document.getElementById('result').innerHTML = eval(result)
+  } else {
+    document.getElementById('result').innerHTML = 'Insira...'
+  }
+}
